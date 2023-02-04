15db9628daab8cd55573e9c08d9364459274e277
kkeas <keaskatrina@gmail.com>
2023-02-04 14:02:08 -0800

    pycodestyle

--- console.py
+++ console.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
 
-import cmd 
+"""console"""
+import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
-    
+
     def do_EOF(self, arg):
         """exits the console"""
-        return True 
+        return True
 
     def help_EOF(self):
-        print ("syntax: EOF"),
-        print ("-- exits console")
+        print("syntax: EOF"),
+        print("-- exits console")
 
     def do_quit(self, arg):
         """exits console"""
         return True
 
     def help_quit(self):
-        print ("syntax: quit"),
-        print ("-- terminates the application")
+        print("syntax: quit"),
+        print("-- terminates the application")
 
     def emptyline(self):
         return False
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
