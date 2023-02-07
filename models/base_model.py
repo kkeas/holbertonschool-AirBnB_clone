efe161e7240df755a27bbaf720e98744d768b692
jobabyyy <joeannaavila123@gmail.com>
2023-02-06 18:55:21 -0600

    trying

--- models/base_model.py
+++ models/base_model.py
@@ -1,38 +1,22 @@
-#!/usr/bin/python3
-"""module base model"""
-
-from uuid import uuid4
+import uuid
 from datetime import datetime
 import models
 
 class BaseModel:
-    """base model"""
-    def __init__(self, *args, **kwargs):
-        """type method init"""
-        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key == "created_at" or "updated_at":
-                    setattr(self, key, datetime.strptime(value, timeformat))
-                elif key != '__class__':
-                    setattr(self, key, value)
-        else:           
-            self.id = str(uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
+    """implementation of arguments"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
 
-        def __str__(self):
-            """ Type method __str__ """
-            class_name = self.__class__.__name__
-            return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-        def save(self):
-            """ Update the attribute update_at with current datetime """
-            self.updated_at = datetime.today
+    def save(self):
+        self.updated_at = datetime.now()
 
-        def to_dict(self):
-            lildict = self.__dict__.copy()
-            lildict["created_at"] = datetime.created_at.isoformat()
-            lildict["updated_at"] = datetime.updated_at.isoformat()
-            lildict["__class__"] = self.__class__.__name__
-            return lildict
+    def to_dict(self):
+        dict_new = self.__dict__.copy()
+        dict_new['__class__'] = self.__class__.__name__
+        dict_new['created_at'] = dict_new['created_at'].isoformat()
+        dict_new['updated_at'] = dict_new['updated_at'].isoformat()
+        return dict_new
