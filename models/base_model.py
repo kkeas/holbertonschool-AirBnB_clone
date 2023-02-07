9e486c2e4edd74da18d12e5ac23ac2c6158dcfe9
jobabyyy <joeannaavila123@gmail.com>
2023-02-07 01:22:43 -0600

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -3,20 +3,24 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
+    """Represent a class BaseModel that defines
+    all attributes/methods for other classes"""
+    
+    
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at":
-                    self.created_at = datetime.strptime(value, t)
-                elif key == "updated_at":
-                    self.updated_at = datetime.strptime(value, t)
-                else:
-                    setattr(self, key, value)
-                    
+    self.id = str(uuid.uuid4())
+    self.created_at = datetime.now()
+    self.updated_at = datetime.now()
+    
+    if kwargs:
+        for key, value in kwargs.items():
+            if key == "created_at":
+                self.created_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            elif key == "updated_at":
+                self.updated_at = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            elif key != "__class__":
+                setattr(self, key, value)
+                
     def __str__(self):
         return "[{}] ({}) {}".format(
             type(self).__name__,
