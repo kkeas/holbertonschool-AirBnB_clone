2e918fb830cf5c99d2c1a2a3ae367dcad9db3e61
jobabyyy <joeannaavila123@gmail.com>
2023-02-08 14:18:01 -0600

    update

--- console.py
+++ console.py
@@ -1,144 +1,275 @@
 #!/usr/bin/python3
-"""console"""
+"""
+Entry point for the AirBNB clone console
+This contains and runs the CMD module and handles th entry
+point for the project
+"""
+
 import cmd
-import json
-from models import storage
+import shlex
+import models
+import re
+import ast
+import inspect
 from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
 from models.city import City
-from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
 
 class HBNBCommand(cmd.Cmd):
-    """console"""
     prompt = '(hbnb) '
-    __classes = {
-        'BaseModel',
-        'User',
-        'Place',
-        'State',
-        'City',
-        'Amenity',
-        'Review'
-        }
+    listOfProjectClass = ["BaseModel", "City", "Place", "Review", "State",
+                          "User", "Amenity"]
+    intAttrs = ["number_rooms", "number_bathrooms", "max_guest",
+                "price_by_night"]
+    floatAttrs = ["latitude", "longitude"]
 
     def do_EOF(self, arg):
-        """Exits the console"""
+        """Exits console"""
         return True
-
-    def help_EOF(self):
-        print("syntax: EOF")
-        print("-- exits console")
-
-    def do_quit(self, arg):
-        """Exits the console"""
-        return True
-
-    def help_quit(self):
-        print("syntax: quit")
-        print("-- terminates the application")
 
     def emptyline(self):
+        """ overwriting the emptyline method """
         return False
 
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
     def do_create(self, arg):
-        """Creates a new instance and saves it to the JSON file"""
-        if not arg:
-            print("** class name missing **")
-        elif arg not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        else:
-            class_dict = {
-                'BaseModel': BaseModel,
-                'User': User,
-                'Place': Place,
-                'State': State,
-                'City': City,
-                'Amenity': Amenity,
-                'Review': Review
-            }
+        """
+****HELP****
+Creates a new instance of a class and prints ID and saves to file.
+Usage: create <class name>
+Usage: <class name>.create()
+"""
+        lineAsArgs = shlex.split(arg)
+        if not self.verify_class_in_project(lineAsArgs):
+            return
+        newInstance = eval(str(lineAsArgs[0]) + '()')
+        print(newInstance.id)
+        newInstance.save()
 
-            new_obj = class_dict[arg]()
-            new_obj.save()
-            print('{}'.format(new_obj.id))
-            storage.save()
+    def do_show(self, arg):
+        """
+****HELP****
+Prints the string representation of an instance
+based on the class name and id.
+Usage: show <class name> <id>
+Usage: <class name>.show(<id>)
+"""
+        lineAsArgs = shlex.split(arg)
+        if not self.verify_class_in_project(lineAsArgs):
+            return
+        if not self.verify_id_exists(lineAsArgs):
+            return
+        objectAsKey = str(lineAsArgs[0]) + '.' + str(lineAsArgs[1])
+        objectsInStorage = models.storage.all()
+        print(objectsInStorage[objectAsKey])
 
-    def do_show(self, line):
-        arg = line.split()
-        obj_dict = storage.all()
-        if len(arg) == 0:
-            print("** class name missing **")
-        elif arg[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        elif len(arg) == 1:
-            print("** instance id missing **")
-        elif "{}.{}".format(arg[0], arg[1]) not in obj_dict:
-            print("** no instance found **")
-        else:
-            print(obj_dict["{}.{}".format(arg[0], arg[1])])
-
-    def do_destroy(self, line):
-        """Destroy instance specified by user; save changes to JSON file"""
-        arg = line.split()
-        obj_dict = storage.all()  # all() method from file_storage.py
-        if len(arg) == 0:
-                print("** class name missing **")
-        elif arg[0] not in HBNBCommand.__classes:
-                print("** class doesn't exist **")
-        elif len(arg) == 1:
-                print("** instance id missing **")
-        elif "{}.{}".format(arg[0], arg[1]) not in obj_dict:
-                print("** no instance found **")
-        else:
-            del (obj_dict["{}.{}".format(arg[0], arg[1])])
-            storage.save()
+    def do_destroy(self, arg):
+        """
+****HELP****
+Deletes an instance based on the class name and id
+Usage: destroy <class name> <id>
+Usage: <class name>.destroy(<id>)
+"""
+        lineAsArgs = shlex.split(arg)
+        if not self.verify_class_in_project(lineAsArgs):
+            return
+        if not self.verify_id_exists(lineAsArgs):
+            return
+        objectAsKey = str(lineAsArgs[0]) + '.' + str(lineAsArgs[1])
+        models.storage.all().pop(objectAsKey)
+        models.storage.save()
 
     def do_all(self, arg):
-        """ Print all objects or all objects of specified class """
-        yes = 0
-        all_obj = [str(v) for v in storage.all().values()]
-        if not arg:
-            yes = 1
-            print('{}'.format(all_obj))
-        elif arg:
-             arg_list = arg.split()
-        if arg and arg_list[0] in HBNBCommand.__classes:
-           yes = 1
-           all_obj = storage.all()  # all() method from file_storage.py
-           name = arg_list[0]
-           all_obj = ([str(v) for k, v in all_obj.items()
-                        if name == v.__class__.__name__])
-        print(all_obj)
-
-        if yes == 0:
-            print("** class doesn't exist **")
+        """
+****HELP****
+Prints list of strings of all instances or specified instances
+Usage: all - Prints every saved object
+Usage: all <class name> - prints every saved object of "class name"
+Usage: <class name>.all()
+"""
+        lineAsArgs = shlex.split(arg)
+        objectsInStorage = models.storage.all()
+        listOfObjectToPrint = []
+        if len(lineAsArgs) == 0:
+            for value in objectsInStorage.values():
+                listOfObjectToPrint.append(str(value))
+        else:
+            if not self.verify_class_in_project(lineAsArgs):
+                return
+            for (key, value) in objectsInStorage.items():
+                if lineAsArgs[0] in key:
+                    listOfObjectToPrint.append(str(value))
+        print(listOfObjectToPrint)
 
     def do_update(self, line):
-        """ Update if given exact object, exact attribute """
-        args = line.split()
-        no_change = ["id", "created_at", "updated_at"]
-        obj_dict = storage.all()  # all() method from file_storage.py
-        if not line:
-            print("** class name missing **")
-        elif args[0] not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
+        """
+****HELP****
+Updates an instance based on the class name and id.
+Usage: update <class name> <id>, <attribute name>, <attribute value>
+Usage: <class name>.update(<id>, <attribute name>, <attribute value>)
+****EXTRA****
+Can take a dictionary as input to update multiple attributes at once
+Usage: update <class name> <id>, <dictionary representation>
+usage: <class name>.update(<id>, <dictionary representation>)"""
+        lineArgs = shlex.split(line)
+        ArgLineDict = None
+        if not self.verify_class_in_project(lineArgs):
+            return
+        if not self.verify_id_exists(lineArgs):
+            return
+        objAsKey = str(lineArgs[0]) + '.' + str(lineArgs[1])
+        if "{" in line:
+            ArgLineDict = self.check_dictionary_exists(line)
+        if ArgLineDict is None:
+            if not self.verify_attribute_arguments(lineArgs):
+                return
+            self.set_Attribute_correctly(objAsKey, lineArgs[2], lineArgs[3])
+        if isinstance(ArgLineDict, dict):
+            for (key, value) in ArgLineDict.items():
+                self.set_Attribute_correctly(objAsKey, key, value)
+        models.storage.all()[objAsKey].save()
+
+    @staticmethod
+    def check_dictionary_exists(line):
+        """ Method checks if update was passed a dictionary"""
+        lineAsArgs = line.split("{")
+        dictionaryInLine = "{" + lineAsArgs[1]
+        try:
+            typeDictionary = ast.literal_eval(dictionaryInLine)
+        except SyntaxError:
+            return None
+        return (typeDictionary)
+
+    @classmethod
+    def set_Attribute_correctly(cls, objAsKey, key, value):
+        """sets the attributes with the correcy casting"""
+        if key in cls.intAttrs:
+            setattr(models.storage.all()[objAsKey], key, int(value))
+        elif key in cls.floatAttrs:
+            setattr(models.storage.all()[objAsKey], key, float(value))
         else:
-            class_id = "{}.{}".format(args[0], args[1])
-        if class_id not in storage.all():
-                print("** no instance found **")
-        elif len(args) < 3:
-                print("** attribute name missing **")
-        elif len(args) < 4:
-                print("** value missing **")
-        elif args[2] not in no_change:
-                obj = obj_dict[class_id]
-                obj.__dict__[args[2]] = args[3]
-                obj.save()
+            setattr(models.storage.all()[objAsKey], key, value)
+
+    def default(self, line):
+        """
+        will ensure input is recycled through to meet contracts
+        outlined in DBC methods
+        """
+        listOfCmdMethods = {"show": self.do_show,
+                            "create": self.do_create,
+                            "update": self.do_update,
+                            "destroy": self.do_destroy,
+                            "all": self.do_all,
+                            "count": self.count_instance}
+        if "." not in line:
+            print("*** unknown syntax: " + line)
+            return
+        if "(" not in line or ")" not in line[-1]:
+            print(line + " *** missing parenthesis")
+            return
+        lineAsArgs = re.findall(r"(.*?)\.(.*?)\((.*?)\)", line)
+        if len(lineAsArgs) == 0:
+            print("*** unknown syntax: " + line)
+            return
+        if lineAsArgs[0][1] not in listOfCmdMethods:
+            print("*** command: " + lineAsArgs[0][1] + " is not reccognised")
+            return
+        className = lineAsArgs[0][0]
+        method = lineAsArgs[0][1]
+        info = lineAsArgs[0][2]
+        argumentString = self.create_argument_string(className, method, info)
+        if argumentString is None:
+            return
+        listOfCmdMethods[method](argumentString)
+
+    @staticmethod
+    def create_argument_string(className, method, info):
+        """ this method is used to create a compatible string argument"""
+        argumentString = className + " "
+        if "," in info:
+            if "{" in info:
+                InfoSplit = re.findall(r'(.*)(\{.*?\})$', info)
+                if len(InfoSplit) == 0:
+                    print("Incorrect dictionary syntax in " + info)
+                    return (None)
+                firstArgument = InfoSplit[0][0].replace(",", "")
+                dictionaryInLine = InfoSplit[0][1]
+                argumentString += firstArgument + dictionaryInLine
+            else:
+                argumentString += info.replace(",", "")
+        else:
+            argumentString += info
+        return (argumentString)
+
+    def help_count(self):
+        print("\n\
+****HELP****\n\
+Will count number of instances of a class.\n\
+Usage: <class name>.count()\n\
+")
+
+    def count_instance(self, arg):
+        """retrieves number of instances of a class"""
+        count = 0
+        instanceStorage = models.storage.all()
+        arg = shlex.split(arg)
+        for (key, value) in instanceStorage.items():
+            if arg[0] in key:
+                count += 1
+        print(count)
+
+    @classmethod
+    def verify_class_for_default(cls, classNameToCheck):
+        """verify that class being created is defined in the project
+        """
+        if classNameToCheck not in cls.listOfProjectClass:
+            return False
+        return True
+
+    @classmethod
+    def verify_class_in_project(cls, args):
+        """verify that class being created is defined in the project
+        """
+        if len(args) == 0:
+            print("** class name missing **")
+            return False
+        if args[0] not in cls.listOfProjectClass:
+            print("** class doesn't exist **")
+            return False
+        return True
+
+    @staticmethod
+    def verify_id_exists(args):
+        """verify that the ID being called exists"""
+        if len(args) < 2:
+            print("** instance id missing **")
+            return False
+        objects = models.storage.all()
+        string_key = str(args[0]) + '.' + str(args[1])
+        if string_key not in objects.keys():
+            print("** no instance found **")
+            return False
+        return True
+
+    @staticmethod
+    def verify_attribute_arguments(args):
+        """verify the attribute argument was passed correctly
+        """
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return False
+        if len(args) < 4:
+            print("** value missing **")
+            return False
+        return True
 
 
 if __name__ == '__main__':
